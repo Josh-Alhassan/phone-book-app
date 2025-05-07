5f27cc0cf88bbea154d0f735fc2c172cdb320ff7
Alhassan Joshua Abel <saintjosh26@gmail.com>
2025-05-07 10:07:41 +0000

    add phone number input

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,63 @@ import './App.css'
 
 function App() {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    { name: 'Arto Hellas', phone: '040-1234567' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newPhone, setNewPhone] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Check if the name already exists
-    if (persons.some(person => person.name === newName)) {
+
+    // Condition to check if name already exits
+    if (persons.some(person => person.name.toLowerCase() === newName.toLowerCase())) {
       alert(`${newName} is already added to phonebook`)
       return
     }
 
     const newPerson = {
-      name: newName
+      name: newName,
+      phone: newPhone
     }
 
     setPersons(persons.concat(newPerson))
     setNewName('');
+    setNewPhone('');
   }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
   return (
     <div className='container'>
       <h2>Phonebook</h2>
 
+      <div>
+        filter shown with: <input value={searchTerm} onChange={handleSearchChange} />
+      </div>
+
       <form onSubmit={handleSubmit}>
         <div>
         <strong>Name:</strong> <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          phone: <input value={newPhone} onChange={handlePhoneChange} />
         </div>
         <div>
           <button type="submit">Add</button>
@@ -43,8 +68,8 @@ function App() {
 
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person, index) => (
-          <li key={index}>{person.name}</li>
+        {filteredPersons.map((person, index) => (
+          <li key={index}>{person.name} - {person.phone} </li>
         ))}
       </ul>
     </div>
